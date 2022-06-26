38eff5c7fde7ddd7e054a09b2b571fc74e5ade80
Josh Marinacci <joshua@marinacci>
2022-06-25 18:15:45 -0700

    more work on consts

--- testit.mjs
+++ testit.mjs
@@ -4,9 +4,13 @@ import fs from 'fs-extra'
 
 console.log("doing stuff here")
 
-let LETTERS = ['A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-const DIGITS = ['0','1','2','3','4','5','6','7','8','9']
-let COMMANDS = ['BACKSPACE','ARROW_LEFT','ARROW_RIGHT','ENTER',]
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split("")
+const DIGITS = "0123456789".split("")
+let COMMANDS = [
+    'BACKSPACE',
+    'arrow_left','arrow_right','arrow_up','arrow_down',
+    'ENTER',
+]
 const PUNC = {
     'PERIOD':'.',
     'COMMA':',',
@@ -15,30 +19,33 @@ const PUNC = {
     'EXCLAMATION_POINT':'!',
     'bracket_left':'[',
     'bracket_right':']',
+    'slash':'/',
+    'backslash':'\\\\',
 }
 
-const to_up = s => s.toUpperCase()
-const logical = `
-export const LOGICAL_KEYBOARD_CODE = {
-${LETTERS.map(l => `   KEY_${l}:'KEY_${l}',`).join("\n")}
-${DIGITS.map(l => `   DIGIT_${l}:'DIGIT_${l}',`).join("\n")}
-${COMMANDS.map(c => `   ${c}:'${c}',`).join("\n")}
-${Object.keys(PUNC).map(p =>     `   ${to_up(p)}:'${to_up(p)}',`).join("\n")}
-}
-`
-
+const up = s => s.toUpperCase()
 function to_DomCase(p) {
     return p.split("_").map(s => {
         return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase()
     }).join("")
 }
 
+const logical = `
+export const LOGICAL_KEYBOARD_CODE = {
+${LETTERS.map(l =>  `   KEY_${up(l)}:'KEY_${up(l)}',`).join("\n")}
+${DIGITS.map(l =>   `   DIGIT_${up(l)}:'DIGIT_${up(l)}',`).join("\n")}
+${COMMANDS.map(c => `   ${up(c)}:'${up(c)}',`).join("\n")}
+${Object.keys(PUNC).map(p =>     `   ${up(p)}:'${up(p)}',`).join("\n")}
+}
+`
+
+
 const dom = `
 export const DOM_KEYBOARD_CODE_TO_LOGICAL_CODE = {
-${LETTERS.map(l => `'Key${l}':LOGICAL_KEYBOARD_CODE.KEY_${l},`).join("\n")}
-${DIGITS.map(l => `'Digit${l}':LOGICAL_KEYBOARD_CODE.DIGIT_${l},`).join("\n")}
-${COMMANDS.map(p =>  `   '${to_DomCase(p)}':LOGICAL_KEYBOARD_CODE.${p},`).join("\n")}
-${Object.keys(PUNC).map(p =>      `   '${to_DomCase(p)}':LOGICAL_KEYBOARD_CODE.${to_up(p)},`).join("\n")}
+${LETTERS.map(l => `'Key${up(l)}':LOGICAL_KEYBOARD_CODE.KEY_${up(l)},`).join("\n")}
+${DIGITS.map(l => `'Digit${up(l)}':LOGICAL_KEYBOARD_CODE.DIGIT_${up(l)},`).join("\n")}
+${COMMANDS.map(c =>  `   '${to_DomCase(c)}':LOGICAL_KEYBOARD_CODE.${up(c)},`).join("\n")}
+${Object.keys(PUNC).map(p =>      `   '${to_DomCase(p)}':LOGICAL_KEYBOARD_CODE.${up(p)},`).join("\n")}
 }
 
 export const DOM_KEYBOARD_KEY_TO_LOGICAL_KEY = {
