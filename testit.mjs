222b641a7a85a0277ea70ae63896d59940405f8c
Josh Marinacci <joshua@marinacci>
2022-06-25 19:10:19 -0700

    update generator for the idealos TypeScript and Rust code

--- testit.mjs
+++ testit.mjs
@@ -36,6 +36,24 @@ function to_DomCase(p) {
         return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase()
     }).join("")
 }
+const LOG_TO_SDL = {
+    'arrow_left':'Left',
+    'arrow_right':'Right',
+    'arrow_up':'Up',
+    'arrow_down':'Down',
+    'escape':'Escape',
+    'shift_left':'LShift', 'shift_right':'RShift',
+    'control_left':'LCtrl', 'control_right':'RCtrl',
+    'alt_left':'LAlt', 'alt_right':'RAlt',
+    'meta_left':'LGui', 'meta_right':'RGui',
+    'enter':'Return',
+}
+
+function to_SDL(p) {
+    console.log("generating to SDL",p)
+    if(LOG_TO_SDL[p]) return LOG_TO_SDL[p]
+    return to_DomCase(p)
+}
 
 const logical = `
 export const LOGICAL_KEYBOARD_CODE = {
@@ -65,4 +83,61 @@ ${Object.entries(PUNC).map(([n,k]) => `'${k}':'${k}',`).join("\n")}
 // console.log("Dom");
 // console.log(dom)
 
+//generate for dom to thneed
 await fs.outputFile('src/generated.ts',logical+dom)
+
+const idealos_out = `
+import {LOGICAL_KEYBOARD_CODE} from "thneed-gfx";
+//cool rad stuff here
+export const IDEALOS_KEYBOARD_CODE = {
+${LETTERS.map(l => `    'LETTER_${up(l)}':LOGICAL_KEYBOARD_CODE.KEY_${up(l)},`).join("\n")}   
+${COMMANDS.map(l => `    '${up(l)}':LOGICAL_KEYBOARD_CODE.${up(l)},`).join("\n")}   
+}
+export const IDEALOS_KEYBOARD_KEY = {
+${LETTERS.map(l => `    'KEY_${up(l)}':'${l.toLowerCase()}',`).join("\n")}   
+
+}
+`
+//generate for idealos to thneed
+await fs.outputFile('../clogwench/apps/common/src/generated.ts',idealos_out)
+
+//generate idealos keyboard enum
+const idealos_rust_out = `
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum KeyCode {
+    RESERVED,
+    UNKNOWN,
+    SPACE,
+
+${LETTERS.map(l  => `    LETTER_${up(l)},`).join("\n")}   
+${DIGITS.map(l   => `    DIGIT_${up(l)},`).join("\n")}   
+${COMMANDS.map(l => `    ${up(l)},`).join("\n")}   
+
+    MOUSE_PRIMARY,
+}
+`
+await fs.outputFile('../clogwench/common/src/generated.rs',idealos_rust_out)
+
+
+const sdl_to_idealos_rust_out = `
+use sdl2::keyboard::Keycode;
+use common::generated::KeyCode;
+
+//hi. cool!
+pub fn sdl_to_common(kc: Keycode) -> KeyCode {
+    println!("converting SDL {}",kc);
+    let code = match kc {
+${LETTERS.map(l =>  `        Keycode::${up(l)} => KeyCode::LETTER_${up(l)},`).join("\n")}   
+${DIGITS.map(l =>   `        Keycode::Num${up(l)} => KeyCode::DIGIT_${up(l)},`).join("\n")}   
+${COMMANDS.map(l => `        Keycode::${to_SDL(l)} => KeyCode::${up(l)},`).join("\n")}   
+        _ => {
+            KeyCode::UNKNOWN
+        }
+    };
+    println!("to code {:?}",code);
+    return code
+}
+`
+await fs.outputFile('../clogwench/plat/native-macos/src/sdl_to_common.rs',sdl_to_idealos_rust_out)
